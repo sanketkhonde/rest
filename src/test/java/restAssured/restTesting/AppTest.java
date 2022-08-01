32e557a1dbd7b0f1ee6f2a7025c51028ae036266
sanket <sanket@SNAKETG3>
2022-08-01 09:46:59 +0530

    rest insanket

---
+++ src/test/java/restAssured/restTesting/AppTest.java
@@ -0,0 +1,20 @@
+package restAssured.restTesting;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
